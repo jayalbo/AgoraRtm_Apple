419652f81472ee399d79c60cccbaf2315981f5a1
Jay Albo <albo.facundo@gmail.com>
2025-04-09 23:13:19 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     products: [
         .library(
             name: "AgoraRTM",
-            targets: ["AgoraRtmKit", "aosl"]
+            targets: ["AgoraRtmKit"]
         ),
     ],
     targets: [
@@ -15,11 +15,6 @@ let package = Package(
             name: "AgoraRtmKit",
             url: "https://download.agora.io/rtmsdk/release/AgoraRtmKit.xcframework_2.2.4.zip",
             checksum: "48993735528efaadc4009478288c32993818a4a23620e2b17f4608c3f78bc9e5"
-        ),
-        .binaryTarget(
-            name: "aosl",
-            url: "https://download.agora.io/rtmsdk/release/aosl.xcframework.zip",
-            checksum: "4d1768316f2738e339627cddb3186bb74b5163241e4e47ffd05e83a41a89027d"
         )
     ]
 )
